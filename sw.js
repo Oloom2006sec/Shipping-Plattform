cad9f88e84d23fe368f70cf12fb0a80b651cf86a
Oloom2006sec <oloom2006@outlook.com>
2026-05-22 16:43:09 +0300

    Remove fetch event listener from service worker

--- sw.js
+++ sw.js
@@ -20,6 +20,3 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
-});
